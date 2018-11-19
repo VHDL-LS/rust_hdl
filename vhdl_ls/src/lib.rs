d2cb03e021d0f9aafeb8f9830f9566766549d98a
Olof Kraigher <olof.kraigher@gmail.com>
2018-11-19 11:01:41 +0100

    Add missing copyright

--- vhdl_ls/src/lib.rs
+++ vhdl_ls/src/lib.rs
@@ -1,3 +1,9 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this file,
+// You can obtain one at http://mozilla.org/MPL/2.0/.
+//
+// Copyright (c) 2018, Olof Kraigher olof.kraigher@gmail.com
+
 mod stdio_server;
 mod vhdl_server;
 pub use stdio_server::start;
