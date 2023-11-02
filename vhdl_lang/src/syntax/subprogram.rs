443f0744ae82c9f0e732b360441fecf09ccff87c
Lukas Scheller <lukas2121@icloud.com>
2023-11-02 22:59:43 +0100

    Fix logic for identifier

--- vhdl_lang/src/syntax/subprogram.rs
+++ vhdl_lang/src/syntax/subprogram.rs
@@ -901,5 +901,19 @@ end procedure swap;
                 semi: code.s1(";").token()
             }
         );
+
+        let code = Code::new("function my_func is new func;");
+        let inst = code.parse_ok_no_diagnostics(parse_subprogram);
+        assert_eq!(
+            inst,
+            Declaration::SubprogramInstantiation(SubprogramInstantiation {
+                kind: WithToken::new(SubprogramKind::Function, code.s1("function").token()),
+                ident: code.s1("my_func").decl_ident(),
+                subprogram_name: code.s1("new func").s1("func").name(),
+                signature: None,
+                generic_map: None,
+                semi: code.s1(";").token()
+            })
+        );
     }
 }
