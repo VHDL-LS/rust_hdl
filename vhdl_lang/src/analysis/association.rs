72595536072d920b5f42a9abcc6ea7b75668cbfc
Olof Kraigher <olof.kraigher@gmail.com>
2023-11-12 10:18:53 +0100

    Move copyright on top

--- vhdl_lang/src/analysis/association.rs
+++ vhdl_lang/src/analysis/association.rs
@@ -1,13 +1,14 @@
-#![allow(clippy::only_used_in_recursion)]
-
-use fnv::FnvHashMap;
-use fnv::FnvHashSet;
-
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this file,
 // You can obtain one at http://mozilla.org/MPL/2.0/.
 //
 // Copyright (c) 2018, Olof Kraigher olof.kraigher@gmail.com
+
+#![allow(clippy::only_used_in_recursion)]
+
+use fnv::FnvHashMap;
+use fnv::FnvHashSet;
+
 use super::analyze::*;
 use super::names::ResolvedName;
 use super::scope::*;
