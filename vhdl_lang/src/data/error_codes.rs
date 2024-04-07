e31cd6ca3edc06956ff61ff05da712321e881a1f
Lukas Scheller <lukas2121@icloud.com>
2024-04-07 16:59:51 +0200

    Add license header

--- vhdl_lang/src/data/error_codes.rs
+++ vhdl_lang/src/data/error_codes.rs
@@ -1,3 +1,9 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this file,
+// You can obtain one at http://mozilla.org/MPL/2.0/.
+//
+// Copyright (c) 2024, Olof Kraigher olof.kraigher@gmail.com
+
 use crate::{Diagnostic, Severity, SrcPos};
 use enum_map::{enum_map, Enum, EnumMap};
 use std::fmt::{Display, Formatter};
