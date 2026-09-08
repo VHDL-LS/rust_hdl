a29c27cb66bc00db8069d2f710c0c47ad4a37ad4
Lukas Scheller <lukas2121@icloud.com>
2026-09-08 08:32:12 +0200

    Fix: configuration declarative part may only take group declarations, not template declarations

--- vhdl_syntax/src/parser/mod.rs
+++ vhdl_syntax/src/parser/mod.rs
@@ -40,7 +40,6 @@ use crate::standard::VHDLStandard;
 use crate::syntax::node::SyntaxNode;
 use crate::syntax::{DesignFileSyntax, NodeKind};
 use crate::tokens::TokenStream;
-use crate::tokens::Tokenizer;
 
 pub(crate) mod builder;
 pub mod error;
@@ -121,7 +120,7 @@ pub(crate) fn parse_syntax_with_standard<T>(
     input: impl IntoIterator<Item = u8>,
     parser_fn: impl FnOnce(&mut Parser) -> T,
 ) -> (SyntaxNode, Vec<error::SyntaxErr>) {
-    let token_stream: TokenStream = Tokenizer::with_standard(standard, input.into_iter()).collect();
+    let token_stream: TokenStream = crate::tokens::Tokenizer::with_standard(standard, input.into_iter()).collect();
     let mut parser = Parser::new(token_stream, standard);
     parser_fn(&mut parser);
     let (green, diagnostics) = parser.end();
