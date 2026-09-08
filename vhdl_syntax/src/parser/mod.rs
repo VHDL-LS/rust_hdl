0b0579ae58c7acfa0ffe1bc5bc82ed157b1dbe93
Lukas Scheller <lukas2121@icloud.com>
2026-09-08 09:38:17 +0200

    fmt

--- vhdl_syntax/src/parser/mod.rs
+++ vhdl_syntax/src/parser/mod.rs
@@ -92,7 +92,7 @@ pub fn parse(token_stream: impl Into<TokenStream>) -> (DesignFileSyntax, Vec<err
 }
 
 /// Parse and return a VHDL file, tokenizing and parsing under the given `standard`.
-/// 
+///
 /// **Note**: This is mostly a placeholder right now. Currently, not much changes
 /// for anything other than VHDL-2008.
 pub fn parse_with_standard(
