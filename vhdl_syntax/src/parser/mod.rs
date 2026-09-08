eb21f4cb577be9f36412c9ebeca4763de56866dc
Lukas Scheller <lukas2121@icloud.com>
2026-09-08 08:34:44 +0200

    format

--- vhdl_syntax/src/parser/mod.rs
+++ vhdl_syntax/src/parser/mod.rs
@@ -120,7 +120,8 @@ pub(crate) fn parse_syntax_with_standard<T>(
     input: impl IntoIterator<Item = u8>,
     parser_fn: impl FnOnce(&mut Parser) -> T,
 ) -> (SyntaxNode, Vec<error::SyntaxErr>) {
-    let token_stream: TokenStream = crate::tokens::Tokenizer::with_standard(standard, input.into_iter()).collect();
+    let token_stream: TokenStream =
+        crate::tokens::Tokenizer::with_standard(standard, input.into_iter()).collect();
     let mut parser = Parser::new(token_stream, standard);
     parser_fn(&mut parser);
     let (green, diagnostics) = parser.end();
